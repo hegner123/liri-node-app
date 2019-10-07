0a4270fa95466c1fe05a6530cd6be35742356525
Michael Hegner <hegner123@gmail.com>
2019-10-07 16:48:26 -0400

    configured and tested switch, tested combine function for paramaters

--- liri.js
+++ liri.js
@@ -4,3 +4,44 @@ var keys = require("./keys.js");
 var spotify = keys.spotify;
 
 console.log(spotify)
+
+var command = process.argv[2];
+var param = combine();
+
+console.log(param);
+
+switch(command) {
+    case "concert-this":
+      test(command, param);
+      break;
+
+    case "spotify-this-song":
+      console.log("spotify-this-song");
+      break;
+
+    case "movie-this":
+      console.log("movie-this");
+      break;
+
+    case "do-what-it-says":
+      console.log("do what it says");
+      break;
+
+    default:
+      console.log("default");
+
+  }
+
+
+
+  function test (a,b){
+    console.log(a + " " + b);
+  }
+
+  function combine(){
+    var array = []
+    for (i=3;i<process.argv.length;i++) {
+      array.push(process.argv[i]);
+    }
+    return array.join(" ");
+  }
