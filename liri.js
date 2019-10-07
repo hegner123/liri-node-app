48ae68e9af15df461c5bb38acfffc5c5bf64b1c0
Michael Hegner <hegner123@gmail.com>
2019-10-07 17:21:54 -0400

    completed spotify-this-song

--- liri.js
+++ liri.js
@@ -1,14 +1,20 @@
 var x = require("dotenv").config();
+var axios = require("axios");
 var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
 
-var spotify = keys.spotify;
 
-console.log(spotify)
+
+
+
+var spotify = new Spotify(keys.spotify);
+
+
 
 var command = process.argv[2];
 var param = combine();
 
-console.log(param);
+
 
 switch(command) {
     case "concert-this":
@@ -16,7 +22,9 @@ switch(command) {
       break;
 
     case "spotify-this-song":
-      console.log("spotify-this-song");
+
+      spotifySearch(param)
+
       break;
 
     case "movie-this":
@@ -43,5 +51,21 @@ switch(command) {
     for (i=3;i<process.argv.length;i++) {
       array.push(process.argv[i]);
     }
-    return array.join(" ");
+    return array.join("-");
+  }
+
+  function spotifySearch(a,b){
+    spotify.search({ type: 'track', query: a }, function(err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+var songArtist = data.tracks.items[0].album.artists[0].name
+var songName = data.tracks.items[0].name
+var songLink = data.tracks.items[0].external_urls.spotify
+var songAlbum = data.tracks.items[0].album.name
+    console.log(songArtist);
+    console.log(songName);
+    console.log(songLink);
+    console.log(songAlbum)
+    });
   }
